6a5bcc2725ecbbc9596aac01bcab0c2f32c533b8
isaiahaquino <isaiahaquino1997@gmail.com>
2023-03-28 21:37:46 -0700

    book appt page setup

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import { TApiAllAvailabilitiesResp, TApiSingleAvailabilityResp } from "@/types"
 import moment from "moment"
+import FormInput from "@/components/FormInput"
 
 export default function Book() {
   const [availability, setAvailability] = useState<TApiAllAvailabilitiesResp>()
@@ -26,15 +27,15 @@ export default function Book() {
   }
 
   return (
-    <div className='flex flex-col gap-[5rem] px-4 mt-10 text-grey'>
+    <div className='flex flex-col gap-[5rem] px-4 mt-[5rem] text-grey'>
       <section className="text-center">
         <h1 className="text-3xl font-serif">Book an Appointment</h1>
       </section>
 
-      <section>
+      <section className="my-10">
         <form className="flex flex-col gap-4">
-          <fieldset className="flex flex-col">
-            <label htmlFor="service">Select a service:</label>
+          <fieldset className="flex flex-col gap-4 bg-grey-dark px-4 py-6 rounded">
+            <label htmlFor="service">Select a service</label>
             <select id="formService" name="service">
               <option value="f">Fade</option>
               <option value="fs">Fade + Shear work</option>
@@ -42,13 +43,26 @@ export default function Book() {
               <option value="fbs">Fade + Beard + Shear work</option>
             </select>
           </fieldset>
-          <fieldset className="flex flex-col">
-            <label htmlFor="date">Select a date:</label>
+          <fieldset className="flex flex-col gap-4 bg-grey-dark px-4 py-6 rounded">
+            <label htmlFor="date">Select a date</label>
             <select id="formDate" name="date" onChange={handleSelectedDate}>
               {availability?.availabilities.map((availability) => (
                 <option key={availability.id}>{moment(availability.date).format("ddd, MMM DD")}</option>
               ))}
             </select>
+          </fieldset>
+            {/* Select Time Component */}
+
+          <fieldset className="flex flex-col gap-4 bg-grey-dark px-4 py-6 rounded">
+            {/* Customer Info */}
+            <h1>Enter your details below</h1>
+            <FormInput attributes={{name: "firstName", label: "First Name", inputId: "formFirstName", type: "text", onChange: () => {}, required: true }} />
+            <FormInput attributes={{name: "lastName", label: "Last Name", inputId: "formLastName", type: "text", onChange: () => {}, required: false }} />
+            <FormInput attributes={{name: "phoneNum", label: "Phone Number", inputId: "formPhoneNum", type: "tel", onChange: () => {}, required: true }} />
+
+          </fieldset>
+          <fieldset>
+            <input type="submit" value="Book Appointment" className="w-full py-3 font-serif font-semibold text-xl bg-yellow px-6 text-grey-dark rounded"/>
           </fieldset>
         </form>
       </section>
---
+++ components/FormInput.tsx
@@ -0,0 +1,31 @@
+interface IFormInput {
+  attributes: {
+    name: string
+    label: string
+    inputId: string
+    type: string
+    onChange: any
+    required: boolean
+  }
+}
+
+export default function FormInput(props:IFormInput) {
+  return (
+    <div className="relative">
+      <label 
+        htmlFor={props.attributes.name}
+        className="absolute top-[-0.5rem] left-3 bg-grey-dark px-2 text-xs"
+      >
+        {props.attributes.label}
+      </label>
+      <input 
+        className="py-2 px-7 w-full h-[3rem] bg-grey-dark border-[2px] border-black rounded invalid:border-red focus:invalid:border-red focus:border-yellow focus:outline-none" 
+        id={props.attributes.inputId} 
+        name={props.attributes.name}
+        type={props.attributes.type}
+        onChange={props.attributes.onChange}
+        required={props.attributes.required}
+      />
+    </div>
+  )
+}
